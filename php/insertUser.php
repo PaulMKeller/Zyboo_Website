84aa1285c1c68bc7d3a0eebefcc53cd03db92247
PaulKeller <paul.keller.it@gmail.com>
2018-07-01 17:43:11 +0800

    Changes to PHP User Insert
    Script updated for correct data base use.

--- php/insertUser.php
+++ php/insertUser.php
@@ -1,13 +1,14 @@
 <?php
 /* Set Connection Credentials */
-$serverName="db680844177.db.1and1.com";
-$uid = "dbo680844177";
-$pwd = "FireWarden1";
+$serverName="db638520808.db.1and1.com";
+$uid = "dbo638520808";
+$pwd = "Register2016!";
+$database = "db638520808";
 $connectionInfo = array( "UID"=>$uid,
                          "PWD"=>$pwd,
-                         "Database"=>"db680844177",
+                         "Database"=>$database,
                          "CharacterSet"=>"UTF-8");
-$connectionInfo = array( "Database"=>"db638520808", "UID"=>"dbo638520808", "PWD"=>"Register2016!");
+//$connectionInfo = array( "Database"=>"db638520808", "UID"=>"dbo638520808", "PWD"=>"Register2016!");
  
 /* Connect using SQL Server Authentication. */
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
@@ -18,11 +19,14 @@ if( $conn === false ) {
 }
  
 /* TSQL Query */
-$postPersonID = $_POST['PersonID'];
-$postLocationID = $_POST['LocationID'];
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$nickName = $_POST['nickName'];
+$telNo = $_POST['telNo'];
+$emailAddr = $_POST['emailAddr'];
 
-$tsql = "EXEC sp_Warden_Insert @PersonID=?, @LocationID=?";
-$params = array($postPersonID, $postLocationID);
+$tsql = "EXEC sp_Zyboo_User_Insert @firstName=?, @lastName=?, @nickName=?, @telNo=?, @emailAddr=?";
+$params = array($firstName, $lastName, $nickName, $telNo, $emailAddr);
 $stmt = sqlsrv_query( $conn, $tsql, $params);
  
 if( $stmt === false ) {
@@ -31,6 +35,7 @@ if( $stmt === false ) {
 }
 
 /* Process results */
+
 $json = array();
 
 do {
@@ -42,6 +47,7 @@ do {
 /* Run the tabular results through json_encode() */
 /* And ensure numbers don't get cast to trings */
 //echo json_encode($json,<code>JSON_NUMERIC_CHECK</code>);
+
 echo json_encode($json);
 
 /* Free statement and connection resources. */
